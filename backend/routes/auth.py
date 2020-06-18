0f784770c0326d8a757340e80650989e52b4b7f1
Alex Hoang <nhuhoang1012@gmail.com>
2020-06-18 12:46:28 -0700

    Add endpoint to create user - Buggy

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,9 +1,26 @@
 from app import app
+from flask import request
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/')
 def hello():
   return 'Hello World'
 
-@app.route('/ping')
-def ping_pong():
-  return 'ponpingg'
+@app.route('/signup/info', methods=['POST'])
+def make_new_user():
+  content = request.get_json()
+  user_info = User_info(
+    first_name = content['first_name'],
+    last_name = content['last_name'],
+    email_address = content['email_address'],
+    user_bio = content['user_bio'],
+    profile_pic = content['profile_pic'],
+  )
+  db.session.add(user_info)
+  db.session.commit()
+  print(userInfo)
+  return "Received user info"
+
+@app.route('/signup/login/', methods=['POST'])
+def user_login():
+  print('whatever')
