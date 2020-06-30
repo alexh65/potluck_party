7ee3dd13ac4da6645ef3a92a382794bcd744ce0b
Alex Hoang <nhuhoang1012@gmail.com>
2020-06-29 22:11:55 -0700

    Refactor firebase code and successully send things to server

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from api.config import BaseConfig
 
 app = Flask(__name__)
+
+CORS(app)
 
 config = BaseConfig()
 app.config.from_object(config)
