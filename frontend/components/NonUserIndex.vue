cccf5cb8a0e5cf46ec7913554713662826edb3a2
Alex Hoang <nhuhoang1012@gmail.com>
2020-07-20 12:08:22 -0700

    Put index in a separate component and start with nuxt auth

---
+++ frontend/components/NonUserIndex.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="banner">
+      <h1>WELCOME TO</h1>
+      <img id="welcome" src="~/assets/icons/logo_mono_name.svg">
+    </div>
+    <p>A simple tool to share your recipe to the whole wide world</p>
+    <div class="buttons">
+      <b-button class="btn-blue" size="lg">
+        <nuxt-link to='/signup'>
+        SIGN UP</nuxt-link>
+        </b-button>
+      <b-button class="btn-blue" size="lg"><nuxt-link to='/login'>
+        LOGIN</nuxt-link></b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'no-nav',
+  head() {
+    return {
+      title: 'PotluckParty | Home'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.banner{
+  width:100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+a {
+  color: white;
+}
+
+#welcome{
+  width: 40%;
+}
+
+h1 {
+  padding: 3vh;
+}
+p{
+  padding: 2vh;
+  font-size: 2vw;
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="main">
-    <div class="banner">
-      <h1>WELCOME TO</h1>
-      <img id="welcome" src="~/assets/icons/logo_mono_name.svg">
+    <div v-if="$auth.loggedIn">
+      Hi
     </div>
-    <p>A simple tool to share your recipe to the whole wide world</p>
-    <div class="buttons">
-      <b-button class="btn-blue" size="lg">
-        <nuxt-link to='/signup'>
-        SIGN UP</nuxt-link>
-        </b-button>
-      <b-button class="btn-blue" size="lg"><nuxt-link to='/login'>
-        LOGIN</nuxt-link></b-button>
+    <div v-else>
+      <NonUserIndex></NonUserIndex>
     </div>
   </div>
 </template>
 
 <script>
+import NonUserIndex from '../components/NonUserIndex'
 export default {
   layout: 'no-nav',
+  components: {
+    NonUserIndex
+  },
   head() {
     return {
       title: 'PotluckParty | Home'
@@ -28,31 +25,8 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  padding: 3vh;
-}
-p{
-  padding: 2vh;
-  font-size: 2vw;
-}
 .main {
   justify-content: center;
   align-items: center;
 }
-.banner{
-  width:100%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
-a {
-  color: white;
-}
-
-#welcome{
-  width: 40%;
-}
-
-
 </style>
